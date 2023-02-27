6c21a12be06386c2d90d4fc7ac599dd82a4500bc
hakonstrom <hakon.satre.strom@nmbu.no>
2023-02-27 14:50:00 +0100

    Initial

---
+++ Haakostr.Lettsok.Advertisements/Controllers/Advertisements.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Haakostr.Lettsok.Advertisements.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Advertisements : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<Advertisements> _logger;
+
+    public Advertisements(ILogger<Advertisements> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetJobs")]
+    public async Task<string> GetJobs()
+    {
+        // Endpoint URL: https://arbeidsplassen.nav.no/public-feed/api/v1/ads
+        // Documentation (Including public key): https://github.com/navikt/pam-public-feed
+        using var client = new HttpClient();
+
+        // Insert public key to header of request.
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ");
+
+        // URL of public API.
+        var jsonUrl = "https://arbeidsplassen.nav.no/public-feed/api/v1/ads";
+
+        // Retrieve JSON from API.
+        string? json = await client.GetStringAsync(jsonUrl);
+
+        return json;
+    }
+}
+
