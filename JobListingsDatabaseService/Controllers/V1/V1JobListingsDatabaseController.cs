a909f15b242552b493eeb1c279c95c35a8ff0114
hakonstrom <haakostr@hiof.no>
2023-03-08 08:25:38 +0100

    Made a JobsListingsDatabaseService. At the moment it’s empty

---
+++ JobListingsDatabaseService/Controllers/V1/V1JobListingsDatabaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobListingsDatabaseService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class V1JobListingsDatabaseController : ControllerBase
+{
+    private readonly ILogger<V1JobListingsDatabaseController> _logger;
+
+    public V1JobListingsDatabaseController(ILogger<V1JobListingsDatabaseController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public void Get()
+    {
+        
+    }
+}
+
