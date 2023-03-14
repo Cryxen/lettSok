4e8542a82f3ce057ab1875a66082ee76ec17b923
hakonstrom <haakostr@hiof.no>
2023-03-14 07:52:56 +0100

    In the middle of making Get() work with V1Result

--- JobListingsDatabaseService/Model/V1/V1Restult.cs
+++ JobListingsDatabaseService/Model/V1/V1Restult.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
 namespace JobListingsDatabaseService.Model.V1
 {
 	public class V1Restult<T>
 	{
 		public List<string> Errors { get; set; } = new List<string>();
 
+		[JsonIgnore]
 		public bool HasErrors => Errors.Any();
 		
 		public T? Value { get; set; }
