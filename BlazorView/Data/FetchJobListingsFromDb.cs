a1e78afe6d36bd94cea3d729168d41f562e79ca7
hakonstrom <haakostr@hiof.no>
2023-04-11 15:06:45 +0200

    Made connection between blazor view and public api service

---
+++ BlazorView/Data/FetchJobListingsFromDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http.Json;
+
+namespace BlazorView.Data
+{
+	public class FetchJobListingsFromDb
+	{
+        private static HttpClient client = new HttpClient();
+
+        public async Task<String> FetchJobListings()
+		{
+
+            string json = await client.GetStringAsync("https://localhost:7223/api/V2/Advertisements/getAdvertisements");
+
+            return json;
+
+     
+        }
+	}
+}
+
--- BlazorView/Program.cs
+++ BlazorView/Program.cs
@@ -14,6 +14,7 @@ public class Program
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<FetchJobListingsFromDb>();
 
         var app = builder.Build();
 
