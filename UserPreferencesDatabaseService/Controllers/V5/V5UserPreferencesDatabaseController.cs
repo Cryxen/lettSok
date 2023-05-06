d2b35d57a8a24864946c3225a8fe40a6ea8a10cc
hakonstrom <haakostr@hiof.no>
2023-05-06 21:44:39 +0200

    Made Logging in UserPreferenceController

--- UserPreferencesDatabaseService/Controllers/V5/V5UserPreferencesDatabaseController.cs
+++ UserPreferencesDatabaseService/Controllers/V5/V5UserPreferencesDatabaseController.cs
@@ -31,7 +31,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("getUsers")]
     public async Task<List<V3User>> Get()
     {
-
+        _logger.LogInformation("Getting all users from Database, time: {time}", DateTimeOffset.Now);
         var responseUsers = await _UserPreferencesDbContext.users
             .Select(user => new V3User
             {
@@ -50,6 +50,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpPost("saveUser")]
     public async Task<V3Result<V3User>> saveUser(V3User userPost)
     {
+        _logger.LogInformation("Saving user with name: {0} to Database, time: {time}", userPost.Name, DateTimeOffset.Now);
         var User = new User()
         {
             Id = Guid.NewGuid(),
@@ -74,6 +75,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("getInterest")]
     public async Task<List<V3Interested>> getInterest()
     {
+        _logger.LogInformation("Getting all interests from Database, time: {time}", DateTimeOffset.Now);
 
         var responseInterests = await _UserPreferencesDbContext.interestedAdvertisements
             .Select(interest => new V3Interested
@@ -94,6 +96,8 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpPost("saveInterest")]
     public async Task<V3Result<V3Interested>> saveInterest (V3Interested interestPost)
     {
+        _logger.LogInformation("Saving interest post consisting of adverstisementUuid: {0}, and UserGuid: {1} to Database, time: {time}", interestPost.AdvertisementUuid, interestPost.UserGuid, DateTimeOffset.Now);
+
         var Interest = new InterestedAdvertisement()
         {
             AdvertisementUuid = interestPost.AdvertisementUuid,
@@ -121,6 +125,8 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpDelete("deleteInterest")]
     public async Task DeleteInterest(Guid UserGuid, string AdvertisementUuid)
     {
+        _logger.LogInformation("Deleting interest consiting of Advertisement Uuid: {0}, and UserGuid: {1} from Database, time: {time}", AdvertisementUuid, UserGuid, DateTimeOffset.Now);
+
         List<V3Interested>interests = await getInterest();
         foreach (var item in interests)
         {
@@ -139,6 +145,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("getUnInterest")]
     public async Task<List<V3Uninterested>> getUninterest()
     {
+        _logger.LogInformation("Getting all Uninterests from Database, time: {time}", DateTimeOffset.Now);
 
         var responseUninterests = await _UserPreferencesDbContext.uninterestedAdvertisements
             .Select(uninterest => new V3Uninterested
@@ -158,8 +165,11 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     /// <param name="uninterestPost">Uuid of advertisement, and guid of user</param>
     /// <returns>Error code</returns>
     [HttpPost("saveUninterest")]
+
     public async Task<V3Result<V3Uninterested>> saveUninterest(V3Uninterested uninterestPost)
     {
+        _logger.LogInformation("Saving uninterest post consisting of adverstisementUuid: {0}, and UserGuid: {1} to Database, time: {time}", uninterestPost.AdvertisementUuid, uninterestPost.UserGuid, DateTimeOffset.Now);
+
         var Interest = new UninterestedAdvertisement()
         {
             AdvertisementUuid = uninterestPost.AdvertisementUuid,
@@ -187,6 +197,8 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpDelete("deleteUninterest")]
     public async Task deleteUninterest(Guid UserGuid, string AdvertisementUuid)
     {
+        _logger.LogInformation("Deleting interest consiting of Advertisement Uuid: {0}, and UserGuid: {1} from Database, time: {time}", AdvertisementUuid, UserGuid, DateTimeOffset.Now);
+
         List<V3Uninterested> uninterests = await getUninterest();
         foreach (var item in uninterests)
         {
@@ -201,6 +213,8 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("getLocations")]
     public async Task<List<V3Location>> getLocations()
     {
+        _logger.LogInformation("Getting all locations from Database, time: {time}", DateTimeOffset.Now);
+
         var responseLocations = await _UserPreferencesDbContext.locations
     .Select(location => new V3Location
     {
@@ -217,6 +231,8 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("getSearchLocations")]
     public async Task<List<V3SearchLocation>> getSearchLocations()
     {
+        _logger.LogInformation("Getting all SearchLocations from Database, time: {time}", DateTimeOffset.Now);
+
         var responseLocations = await _UserPreferencesDbContext.searchLocations
         .Select(searchLocation => new V3SearchLocation
         {
@@ -231,6 +247,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpPost("saveSearchLocation")]
     public async Task<V3Result<V3SearchLocation>> saveSearchLocation (V3SearchLocation v3SearchLocation)
     {
+        _logger.LogInformation("Save Search Location with location ID {0}, and UserId: {1} to Database, at time: {time}", v3SearchLocation.LocationId, v3SearchLocation.UserId, DateTimeOffset.Now);
         var searchLocation = new SearchLocation()
         {
             UserId = v3SearchLocation.UserId,
@@ -256,6 +273,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpGet("updateLocationsFromGeoNorge")]
     public async Task getMunipalitiesFromPublicAPI()
     {
+        _logger.LogInformation("Fetch municipalities from geonorge, time: {time}", DateTimeOffset.Now);
         IEnumerable<V3Location> locations = new List<V3Location>();
 
         var jsonUrl = "https://ws.geonorge.no/kommuneinfo/v1/kommuner";
@@ -275,7 +293,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
 
     private async Task<V3Result<V3Location>> saveLocation(V3Location v3Location)
     {
-
+        _logger.LogInformation("Saving Municipality {0} to Database, time: {time}", v3Location.Municipality, DateTimeOffset.Now);
         var location = new Data.Location()
         {
             Municipality = v3Location.Municipality,
@@ -295,6 +313,7 @@ public class V5UserPreferencesDatabaseController : ControllerBase
     [HttpDelete("deleteSearchLocation")]
     public async Task deleteSearchLocation(Guid UserId, int locationId)
     {
+        _logger.LogInformation("Deleting Search location with Location id: {0}, and User Id: {1} from Database, time: {time}", locationId, UserId, DateTimeOffset.Now);
         List<V3SearchLocation> searchLocations = await getSearchLocations();
         foreach (var item in searchLocations)
         {
